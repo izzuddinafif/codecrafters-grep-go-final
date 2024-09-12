fe08bfeec71344f9d1d6c37c1b54008ab585307f
afif <izzuddinafif@gmail.com>
2024-09-12 08:16:49 +0700

    [any message]

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 	"unicode"
 )
 
@@ -33,14 +34,15 @@ func main() {
 		fmt.Println("match not found")
 		os.Exit(1)
 	}
-	fmt.Println("match found: ", len(matches), "match(es)")
+	fmt.Println("match found:", len(matches), "match(es)")
 	fmt.Println("match(es):", string(matches))
 }
 
 func matchLine(line []byte, pattern string) bool {
 
 	for len(line) > 0 {
-		if matchHere(line, pattern) {
+		if matched, consumed := matchHere(line, pattern); matched && consumed == len(pattern) {
+			fmt.Println("full match was found")
 			return true
 		}
 		line = line[1:]
@@ -49,12 +51,19 @@ func matchLine(line []byte, pattern string) bool {
 	return false
 }
 
-func matchHere(line []byte, pattern string) bool {
+func matchHere(line []byte, pattern string) (bool, int) {
 
 	fmt.Println("matching", string(line), "vs", pattern)
 	if len(pattern) == 0 {
 		fmt.Println("empty pattern match")
-		return true
+		return true, 0
+	}
+
+	if len(pattern) > 1 && pattern[0] == '[' {
+		end := strings.LastIndex(pattern, "]")
+
+		subMatched, subConsumed := matchHere(line[1:], pattern[end+1:])
+		return subMatched, subConsumed + end + 1
 	}
 
 	if len(pattern) > 1 && pattern[0] == '\\' {
@@ -63,25 +72,25 @@ func matchHere(line []byte, pattern string) bool {
 			if len(line) > 0 && unicode.IsDigit(rune(line[0])) {
 				fmt.Println("digit match :", string(line[0]))
 				matches = append(matches, line[0])
-				return true
+				subMatched, subConsumed := matchHere(line[1:], pattern[2:])
+				return subMatched, subConsumed + 2
 			}
 		case 'w':
 			if len(line) > 0 && (unicode.IsLetter(rune(line[0])) || unicode.IsDigit(rune(line[0])) || line[0] == '_') {
 				fmt.Println("word match :", string(line[0]))
 				matches = append(matches, line[0])
-				return true
+				subMatched, subConsumed := matchHere(line[1:], pattern[2:])
+				return subMatched, subConsumed + 2
 			}
-		default:
-			goto jmp
 		}
-		return false
+		return false, 0
 	}
-jmp:
 	if len(line) > 0 && (pattern[0] == '.' || pattern[0] == line[0]) {
 		fmt.Println("direct match = ", string(line[0]))
 		matches = append(matches, line[0])
-		return matchHere(line[1:], pattern[1:])
+		subMatched, subConsumed := matchHere(line[1:], pattern[1:])
+		return subMatched, subConsumed + 1
 	}
 	fmt.Println("no match")
-	return false
+	return false, 0
 }
